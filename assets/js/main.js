b24c37983fa2db952b6c410b894ace60cf107672
Rodriguez Geoffrey <rodriguezgeoffrey.becode@gmail.com>
2020-02-13 10:05:53 +0100

    add carousel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 (async()=>{
     const responsePopular = await fetch("https://api.themoviedb.org/3/movie/popular?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    const dataP = await responsePopular.json(); console.log(dataP);
+    const dataP = await responsePopular.json();
     const arrP = dataP.results; console.log(arrP);
     const filmsP = Array.of(arrP[0],arrP[1],arrP[2]);
     console.log("filmsP",filmsP);
@@ -20,8 +20,8 @@
     }).join("");
 
     
-    const reponseTopRated = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    const dataT = await reponseTopRated.json(); console.log(dataT);
+    const responseTopRated = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
+    const dataT = await responseTopRated.json();
     const arrT = dataT.results; console.log(arrT);
     const filmsT = Array.of(arrT[0],arrT[1],arrT[2]);
     console.log("filmsT",filmsT);
@@ -40,4 +40,51 @@
                 </div>`
     }).join("");
 
+    const responseUpcoming = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
+    const dataU = await responseUpcoming.json();
+    const arrU = dataU.results;
+    const filmU1 = Array.of(arrU[0]);
+    console.log('filmU1',filmU1);
+
+    document.querySelector("#U1").innerHTML = 
+    filmU1.map((filmU1)=>{
+        return `<div class="carousel-item active">
+                    <img class="d-block w-100" src="https://image.tmdb.org/t/p/w500/${filmU1.backdrop_path}" alt="${filmU1.title}">
+                    <div class="carousel-caption d-none d-md-block ">
+                        <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
+                        <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
+                        <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
+                    </div>
+                </div>`
+    }).join("");
+
+    const filmU2 = Array.of(arrU[1]);
+    console.log('filmU2',filmU2);
+
+    document.querySelector("#U1").innerHTML = 
+    filmU2.map((filmU2)=>{
+        return `<div class="carousel-item active">
+                    <img class="d-block w-100" src="https://image.tmdb.org/t/p/w500/${filmU2.backdrop_path}" alt="${filmU2.title}">
+                    <div class="carousel-caption d-none d-md-block ">
+                        <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
+                        <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
+                        <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
+                    </div>
+                </div>`
+    }).join("");
+
+    const filmU3 = Array.of(arrU[2]);
+    console.log('filmU3',filmU3);
+
+    document.querySelector("#U3").innerHTML = 
+    filmU3.map((filmU3)=>{
+        return `<div class="carousel-item active">
+                    <img class="d-block w-100" src="https://image.tmdb.org/t/p/w500/${filmU3.backdrop_path}" alt="${filmU3.title}">
+                    <div class="carousel-caption d-none d-md-block ">
+                        <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
+                        <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
+                        <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
+                    </div>
+                </div>`
+    }).join("");
 })()
