664f1574479fca4a3f7203a68957f05b6da427b8
Lena B <benoit.lena@protonmail.com>
2020-02-16 13:42:50 +0100

    modif bar nav, couleur, taille page, ombre,...

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,7 +23,7 @@
     const responseTopRated = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
     const dataT = await responseTopRated.json();
     const arrT = dataT.results; console.log(arrT);
-    const filmsT = Array.of(arrT[0],arrT[1],arrT[2], arrT[3], arrT[4]);
+    const filmsT = Array.of(arrT[0],arrT[1],arrT[2], arrT[3]);
     console.log("filmsT",filmsT);
 
     document.querySelector("#TopRated").innerHTML =
@@ -31,7 +31,7 @@
         return `<div class="card">
                 <img src="https://image.tmdb.org/t/p/w500/${filmT.poster_path}" class="card-img-top" alt="${filmT.title}">
                 <div class="card-body">
-                    <h5 class="card-title overflow-auto" style="height:75px">${filmT.original_title}</h5>
+                    <h5 class="card-title overflow-auto" style="height:90px">${filmT.original_title}</h5>
                     <p class="card-text overflow-auto" style="height: 240px">${filmT.overview}</p>
                 </div>
                 <div class="card-footer">
@@ -54,71 +54,7 @@
                 </div>`
     }).join("");
 
-    // const responseUpcoming = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    // const dataU = await responseUpcoming.json();
-    // const arrU = dataU.results;
-    // const filmU1 = Array.of(arrU[0]);
-    // console.log('filmU1',filmU1);
-
-    // document.querySelector("#U1").innerHTML = 
-    // filmU1.map((filmU1)=>{
-    //     return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU1.backdrop_path}" alt="First Slide">
-    //             <div class="carousel-caption d-none d-md-block ">
-    //                 <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
-    //                 <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
-    //                 <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
-    //             </div>`
-    // }).join("");
-
-    // const filmU2 = Array.of(arrU[1]);
-    // console.log('filmU2',filmU2);
-
-    // document.querySelector("#U2").innerHTML = 
-    // filmU2.map((filmU2)=>{
-    //     return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU2.backdrop_path}" alt="Second Slide">
-    //             <div class="carousel-caption d-none d-md-block ">
-    //                 <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
-    //                 <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
-    //                 <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
-    //             </div>`
-    // }).join("");
-
-    // const filmU3 = Array.of(arrU[2]);
-    // console.log('filmU3',filmU3);
-
-    // document.querySelector("#U3").innerHTML = 
-    // filmU3.map((filmU3)=>{
-    //     return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU3.backdrop_path}" alt="Third Slide">
-    //             <div class="carousel-caption d-none d-md-block ">
-    //                 <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
-    //                 <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
-    //                 <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
-    //             </div>`
-    // }).join("");
-
-
-
-
-    // const reponseTopRated2 = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    // const dataT2 = await reponseTopRated2.json(); console.log(dataT);
-    // const arrT2 = dataT2.results; console.log(arrT);
-    // const filmsT2 = Array.of(arrT2[0],arrT2[1],arrT2[2], arrT2[3], arrT2[4]);
-    // console.log("filmsT2",filmsT2);
-
-    // document.querySelector("#TopRated2").innerHTML =
-    // filmsT2.map((filmT2)=>{
-    //     return `<div class="card">
-    //             <img src="https://image.tmdb.org/t/p/w500/${filmT2.poster_path}" class="card-img-top" alt="${filmT2.title}">
-    //             <div class="card-body">
-    //                 <h5 class="card-title overflow-auto" style="height:75px">${filmT2.original_title}</h5>
-    //                 <p class="card-text overflow-auto" style="height: 190px">${filmT2.overview}</p>
-    //             </div>
-    //             <div class="card-footer">
-    //                 <small class="text-muted">${filmT2.release_date}</small>
-    //             </div>
-    //             </div>`
-    // }).join("");
-
+  
 
 
 
@@ -127,6 +63,7 @@
     const dataO = await reponseNowPlaying.json(); 
     const arrO = dataO.results; console.log(arrO);
     const filmsO = Array.of(arrO[0],arrO[1],arrO[2], arrO[3], arrO[4]);
+    const filmsO2 = Array.of(arrO[0],arrO[1],arrO[2], arrO[3]);
     console.log("filmsO",filmsO);
 
     
@@ -147,7 +84,7 @@
     }).join("");
 
     document.querySelector("#NowPlaying2").innerHTML =
-    filmsO.map((filmO)=>{
+    filmsO2.map((filmO)=>{
         return `<div class="card">
                 <img src="https://image.tmdb.org/t/p/w500/${filmO.poster_path}" class="card-img-top" alt="${filmO.title}">
                 <div class="card-body">
