6a478a288f9f34d091018cf9a7ea141ee6993cba
Lena B <benoit.lena@protonmail.com>
2020-02-16 01:21:02 +0100

    refonte + newsletter

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,7 +11,7 @@
                 <img src="https://image.tmdb.org/t/p/w500/${filmP.poster_path}" class="card-img-top" alt="${filmP.title}">
                 <div class="card-body">
                     <h5 class="card-title overflow-auto" style="height:75px">${filmP.original_title}</h5>
-                    <p class="card-text overflow-auto" style="height: 100px">${filmP.overview}</p>
+                    <p class="card-text overflow-auto" style="height: 240px">${filmP.overview}</p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">${filmP.release_date}</small>
@@ -32,7 +32,7 @@
                 <img src="https://image.tmdb.org/t/p/w500/${filmT.poster_path}" class="card-img-top" alt="${filmT.title}">
                 <div class="card-body">
                     <h5 class="card-title overflow-auto" style="height:75px">${filmT.original_title}</h5>
-                    <p class="card-text overflow-auto" style="height: 100px">${filmT.overview}</p>
+                    <p class="card-text overflow-auto" style="height: 240px">${filmT.overview}</p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">${filmT.release_date}</small>
@@ -40,66 +40,84 @@
                 </div>`
     }).join("");
 
-    const responseUpcoming = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    const dataU = await responseUpcoming.json();
-    const arrU = dataU.results;
-    const filmU1 = Array.of(arrU[0]);
-    console.log('filmU1',filmU1);
-
-    document.querySelector("#U1").innerHTML = 
-    filmU1.map((filmU1)=>{
-        return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU1.backdrop_path}" alt="First Slide">
-                <div class="carousel-caption d-none d-md-block ">
-                    <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
-                    <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
-                    <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
-                </div>`
-    }).join("");
-
-    const filmU2 = Array.of(arrU[1]);
-    console.log('filmU2',filmU2);
-
-    document.querySelector("#U1").innerHTML = 
-    filmU2.map((filmU2)=>{
-        return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU2.backdrop_path}" alt="Second Slide">
-                <div class="carousel-caption d-none d-md-block ">
-                    <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
-                    <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
-                    <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
-                </div>`
-    }).join("");
-
-    const filmU3 = Array.of(arrU[2]);
-    console.log('filmU3',filmU3);
-
-    document.querySelector("#U3").innerHTML = 
-    filmU3.map((filmU3)=>{
-        return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU3.backdrop_path}" alt="Third Slide">
-                <div class="carousel-caption d-none d-md-block ">
-                    <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
-                    <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
-                    <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
-                </div>`
-    }).join("");
-    const reponseTopRated2 = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    const dataT2 = await reponseTopRated2.json(); console.log(dataT);
-    const arrT2 = dataT2.results; console.log(arrT);
-    const filmsT2 = Array.of(arrT2[0],arrT2[1],arrT2[2], arrT2[3], arrT2[4]);
-    console.log("filmsT2",filmsT2);
-
-    document.querySelector("#TopRated2").innerHTML =
-    filmsT2.map((filmT2)=>{
+    document.querySelector("#TopRated2").innerHTML = 
+    filmsP.map((filmP)=>{
         return `<div class="card">
-                <img src="https://image.tmdb.org/t/p/w500/${filmT2.poster_path}" class="card-img-top" alt="${filmT2.title}">
+                <img src="https://image.tmdb.org/t/p/w500/${filmP.poster_path}" class="card-img-top" alt="${filmP.title}">
                 <div class="card-body">
-                    <h5 class="card-title overflow-auto" style="height:75px">${filmT2.original_title}</h5>
-                    <p class="card-text overflow-auto" style="height: 100px">${filmT2.overview}</p>
+                    <h5 class="card-title overflow-auto" style="height:75px">${filmP.original_title}</h5>
+                    <p class="card-text overflow-auto" style="height: 240px">${filmP.overview}</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">${filmT2.release_date}</small>
+                    <small class="text-muted">${filmP.release_date}</small>
                 </div>
                 </div>`
     }).join("");
+
+    // const responseUpcoming = await fetch("https://api.themoviedb.org/3/movie/upcoming?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
+    // const dataU = await responseUpcoming.json();
+    // const arrU = dataU.results;
+    // const filmU1 = Array.of(arrU[0]);
+    // console.log('filmU1',filmU1);
+
+    // document.querySelector("#U1").innerHTML = 
+    // filmU1.map((filmU1)=>{
+    //     return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU1.backdrop_path}" alt="First Slide">
+    //             <div class="carousel-caption d-none d-md-block ">
+    //                 <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
+    //                 <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
+    //                 <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
+    //             </div>`
+    // }).join("");
+
+    // const filmU2 = Array.of(arrU[1]);
+    // console.log('filmU2',filmU2);
+
+    // document.querySelector("#U2").innerHTML = 
+    // filmU2.map((filmU2)=>{
+    //     return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU2.backdrop_path}" alt="Second Slide">
+    //             <div class="carousel-caption d-none d-md-block ">
+    //                 <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
+    //                 <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
+    //                 <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
+    //             </div>`
+    // }).join("");
+
+    // const filmU3 = Array.of(arrU[2]);
+    // console.log('filmU3',filmU3);
+
+    // document.querySelector("#U3").innerHTML = 
+    // filmU3.map((filmU3)=>{
+    //     return `<img class="d-block w-100 h-50" src="https://image.tmdb.org/t/p/w500/${filmU3.backdrop_path}" alt="Third Slide">
+    //             <div class="carousel-caption d-none d-md-block ">
+    //                 <h1 class="p-2 font-weight-bold text-shadow">LATEST <strong class="text-danger">ON</strong>LINE <strong class="text-danger">MO</strong>VIES</h1>
+    //                 <p class="p-2 font-weight-bold ">IN SPACE NO ONE CAN HEAR YOU SCREAM</p>
+    //                 <button class="m-3 bg-danger border-0  p-1 text-white rounded ">WATCH TRAILER</button>
+    //             </div>`
+    // }).join("");
+
+
+
+
+    // const reponseTopRated2 = await fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
+    // const dataT2 = await reponseTopRated2.json(); console.log(dataT);
+    // const arrT2 = dataT2.results; console.log(arrT);
+    // const filmsT2 = Array.of(arrT2[0],arrT2[1],arrT2[2], arrT2[3], arrT2[4]);
+    // console.log("filmsT2",filmsT2);
+
+    // document.querySelector("#TopRated2").innerHTML =
+    // filmsT2.map((filmT2)=>{
+    //     return `<div class="card">
+    //             <img src="https://image.tmdb.org/t/p/w500/${filmT2.poster_path}" class="card-img-top" alt="${filmT2.title}">
+    //             <div class="card-body">
+    //                 <h5 class="card-title overflow-auto" style="height:75px">${filmT2.original_title}</h5>
+    //                 <p class="card-text overflow-auto" style="height: 190px">${filmT2.overview}</p>
+    //             </div>
+    //             <div class="card-footer">
+    //                 <small class="text-muted">${filmT2.release_date}</small>
+    //             </div>
+    //             </div>`
+    // }).join("");
 
 
 
@@ -119,33 +137,92 @@
                 <img src="https://image.tmdb.org/t/p/w500/${filmO.poster_path}" class="card-img-top" alt="${filmO.title}">
                 <div class="card-body">
                     <h5 class="card-title overflow-auto" style="height:75px">${filmO.original_title}</h5>
-                    <p class="card-text overflow-auto" style="height: 100px">${filmO.overview}</p>
+                    <p class="card-text overflow-auto" style="height: 240px">${filmO.overview}</p>
                 </div>
                 <div class="card-footer">
                     <small class="text-muted">${filmO.release_date}</small>
                 </div>
-                </div>`
+                </div>
+                `
     }).join("");
-
-    const reponseNowPlaying2 = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
-    const dataO2 = await reponseNowPlaying2.json(); 
-    const arrO2 = dataO2.results; console.log(arrO);
-    const filmsO2 = Array.of(arrO2[0],arrO2[1],arrO2[2], arrO2[3], arrO2[4]);
-    console.log("filmsO",filmsO);
 
     document.querySelector("#NowPlaying2").innerHTML =
-    filmsO.map((filmO2)=>{
+    filmsO.map((filmO)=>{
         return `<div class="card">
-                <img src="https://image.tmdb.org/t/p/w500/${filmO2.poster_path}" class="card-img-top" alt="${filmO2.title}">
+                <img src="https://image.tmdb.org/t/p/w500/${filmO.poster_path}" class="card-img-top" alt="${filmO.title}">
                 <div class="card-body">
-                    <h5 class="card-title overflow-auto" style="height:75px">${filmO2.original_title}</h5>
-                    <p class="card-text overflow-auto" style="height: 100px">${filmO2.overview}</p>
+                    <h5 class="card-title overflow-auto" style="height:75px">${filmO.original_title}</h5>
+                    <p class="card-text overflow-auto" style="height: 240px">${filmO.overview}</p>
                 </div>
                 <div class="card-footer">
-                    <small class="text-muted">${filmO2.release_date}</small>
+                    <small class="text-muted">${filmO.release_date}</small>
                 </div>
-                </div>`
+                </div>
+                `
     }).join("");
+
+    // const reponseNowPlaying2 = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=d8bf019d0cca372bd804735f172f67e8&language=fr-BE&page=1");
+    // const dataO2 = await reponseNowPlaying2.json(); 
+    // const arrO2 = dataO2.results; console.log(arrO);
+    // const filmsO2 = Array.of(arrO2[0],arrO2[1],arrO2[2], arrO2[3], arrO2[4]);
+    // console.log("filmsO2",filmsO2);
+
+    // document.querySelector("#NowPlaying2").innerHTML =
+    // filmsO2.map((filmO2)=>{
+    //     return `<div class="card">
+    //             <img src="https://image.tmdb.org/t/p/w500/${filmO2.poster_path}" class="card-img-top" alt="${filmO2.title}">
+    //             <div class="card-body">
+    //                 <h5 class="card-title overflow-auto" style="height:75px">${filmO2.original_title}</h5>
+    //                 <p class="card-text overflow-auto" style="height: 190px">${filmO2.overview}</p>
+    //             </div>
+    //             <div class="card-footer">
+    //                 <small class="text-muted">${filmO2.release_date}</small>
+    //             </div>
+    //             </div>`
+    // }).join("");
 
 
 })()
+
+
+
+function validation() {
+    let firstname = document.getElementById("form_firstname").value;
+    let lastname = document.getElementById("form_lastname").value; 
+    let email = document.getElementById("form_email").value; 
+    
+    let subject = document.getElementById("form_subject").value;
+    let message = document.getElementById("form_message").value;
+    
+    
+    $('#myModal').modal('toggle');
+    document.querySelector(".firstname").innerHTML = firstname;
+    document.querySelector(".lastname").innerHTML = lastname;
+    document.querySelector(".email").innerHTML = email;
+    document.querySelector(".subject").innerHTML = subject;
+    document.querySelector(".message").innerHTML = message;
+    
+    }
+    document.getElementById('sendmodal').addEventListener('click',validation);
+
+
+
+    //Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
